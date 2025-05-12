8abe942239b7c39da702d28ddb6fcd834a7e63b1
kloihoferL <150671682+kloihoferL@users.noreply.github.com>
2025-05-12 12:04:00 +0200

    Delete contentful-vue directory

--- contentful-vue/src/contentful.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'contentful'
-
-const client = createClient({
-    space: 'wx7pfl2qpv8l',
-    accessToken: 'sHT3M9QeHO8i6XQetIH4SGsG8AJeaJrUnufyL4cEaAU'
-})
-
-export default client

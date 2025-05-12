944102fcc933b6ee3ef8e924a5fdfe1f0ea5f6d3
kloihoferL <150671682+kloihoferL@users.noreply.github.com>
2025-05-12 12:03:51 +0200

    Delete storyblok-vue directory

--- storyblok-vue/src/pages/About.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import { useStoryblok } from '@storyblok/vue';
-const story = await useStoryblok('home', { version: 'published' });
-</script>
-
-<template>
-  <StoryblokComponent v-if="story" :blok="story.content" />
-</template>
